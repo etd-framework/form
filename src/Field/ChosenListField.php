fd0b96edec3a06c850b7c525beb0e561fddc48f2
jbanety <jbanety@gmail.com>
2015-02-24 16:17:25 +0100

    Ajout ChosenListField et InlineRadioField

---
+++ src/Field/ChosenListField.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Part of the ETD Framework Form Package
+ *
+ * @copyright   Copyright (C) 2015 ETD Solutions, SARL Etudoo. Tous droits réservés.
+ * @license     Apache License 2.0; see LICENSE
+ * @author      ETD Solutions http://etd-solutions.com
+ */
+
+namespace EtdSolutions\Form\Field;
+
+class ChosenListField extends \Joomla\Form\Field\TextField {
+
+    protected $type = 'ChosenList';
+
+    protected function getInput() {
+
+        $doc = \EtdSolutions\Document\Document::getInstance();
+
+        $min = ".min";
+        if (JDEBUG) {
+            $min = "";
+        }
+
+        $doc->addRequireJSModule("chosen", "vendor/etdsolutions/chosen/chosen".$min, true, array("jquery"))
+            ->addDomReadyJS("$('.chosen-select').chosen();", false, "chosen, css!vendor/etdsolutions/chosen/chosen".$min.".css");
+
+        if ($this->element['class']) {
+            $this->element['class'] .= " chosen-select";
+        } else {
+            $this->element['class'] = "chosen-select";
+        }
+
+        return parent::getInput();
+    }
+
+}
---
+++ src/Field/InlineRadioField.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Part of the ETD Framework Form Package
+ *
+ * @copyright   Copyright (C) 2015 ETD Solutions, SARL Etudoo. Tous droits réservés.
+ * @license     Apache License 2.0; see LICENSE
+ * @author      ETD Solutions http://etd-solutions.com
+ */
+
+namespace EtdSolutions\Form\Field;
+
+class InlineRadioField extends \Joomla\Form\Field\RadioField {
+
+    /**
+     * The form field type.
+     *
+     * @var    string
+     * @since  1.0
+     */
+    protected $type = 'InlineRadio';
+
+    protected function getInput() {
+
+        $html = array();
+
+        // Initialize some field attributes.
+        $class = $this->element['class'] ? ' class="radio ' . (string) $this->element['class'] . '"' : ' class="radio"';
+
+        // Start the radio field output.
+        $html[] = '<span id="' . $this->id . '"' . $class . '>';
+
+        // Get the field options.
+        $options = $this->getOptions();
+
+        // Build the radio field output.
+        foreach ($options as $i => $option) {
+            // Initialize some option attributes.
+            $checked = ((string) $option->value == (string) $this->value) ? ' checked="checked"' : '';
+            $class = !empty($option->class) ? ' class="' . $option->class . '"' : '';
+            $disabled = !empty($option->disable) ? ' disabled="disabled"' : '';
+
+            // Initialize some JavaScript option attributes.
+            $onclick = !empty($option->onclick) ? ' onclick="' . $option->onclick . '"' : '';
+
+            $html[] = '<label class="radio-inline" for="' . $this->id . $i . '">';
+
+            $html[] = '<input type="radio" id="' . $this->id . $i . '" name="' . $this->name . '"' . ' value="'
+                . htmlspecialchars($option->value, ENT_COMPAT, 'UTF-8') . '"' . $checked . $class . $onclick . $disabled . '>';
+
+            $html[] = ' ' . $option->text;
+
+            $html[] = '</label>';
+        }
+
+        // End the radio field output.
+        $html[] = '</span>';
+
+        return implode($html);
+    }
+}
