c768c380c9632ff31f67f16c1f8403b249e69f49
Jean-Baptiste Alleaume <jbanety@gmail.com>
2016-02-15 16:50:45 +0100

    update Select2

--- src/Field/Select2ListField.php
+++ src/Field/Select2ListField.php
@@ -17,9 +17,31 @@ class Select2ListField extends \Joomla\Form\Field\ListField {
 
     protected function getInput() {
 
+        $options = [];
+
+        if (isset($this->element['templateResult'])) {
+            $options['templateResult'] = (string) $this->element['templateResult'];
+        }
+
+        if (isset($this->element['placeholder'])) {
+            $options['placeholder'] = (string) $this->element['placeholder'];
+        }
+
+        if (isset($this->element['tags'])) {
+            $options['tags'] = (bool) ((string) $this->element['tags'] == "true");
+        }
+
+        if (isset($this->element['allowClear'])) {
+            $options['allowClear'] = (bool) ((string) $this->element['allowClear'] == "true");
+        }
+
+        $options = json_encode($options);
+        $options = str_replace(['"##STARTFUNC##', '##ENDFUNC##"'], "", $options);
+        $options = str_replace("\\\\", "\\", $options);
+
         (new RequireJSUtility())
             ->addRequirePackage('select2', 'js/vendor/select2', 'select2.min')
-            ->addDomReadyJS("$('#" . $this->id . "').select2({templateResult:function(s){return(s.text.indexOf('||')!==-1?$('<span>'+s.text.split('||').join('</span><span>')+'</span>'):s.text)}});", false, "select2");
+            ->addDomReadyJS("$('#" . $this->id . "').select2(" . $options . ");", false, "select2");
 
         return parent::getInput();
     }
