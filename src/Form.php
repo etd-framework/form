8dff3fa56cc9874885f2f236e2b1d1b9161ee0c5
Jean-Baptiste Alleaume <jbanety@gmail.com>
2016-08-12 15:10:21 +0200

    validateField en public

--- src/Form.php
+++ src/Form.php
@@ -105,7 +105,7 @@ class Form extends JoomlaForm implements ContainerAwareInterface {
 
     }
 
-    protected function validateField(\SimpleXMLElement $element, $group = null, $value = null, Registry $input = null)
+    public function validateField(\SimpleXMLElement $element, $group = null, $value = null, Registry $input = null)
     {
         $valid = true;
 
